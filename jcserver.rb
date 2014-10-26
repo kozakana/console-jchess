b4b9dbbca24e6a0530ae006bc1428f04fc4b7ac6
kozakana <goki727@gmail.com>
2014-10-26 21:19:16 +0900

    使わないオプション削除

--- jcserver.rb
+++ jcserver.rb
@@ -9,9 +9,6 @@ def cmdline
     parser.on('-i [VALUE]', '--ipaddr [VALUE]', '引数付きオプション(デフォルトlocalhost)') do |val|
       args[:ipaddr]=val if val!=nil
     end
-    parser.on('-p [VALUE]', '--port [VALUE]', 'ポート番号指定(デフォルト1117)') do |val|
-      args[:port]=val if val!=nil
-    end
     parser.parse!(ARGV)
   end 
   args
@@ -19,7 +16,6 @@ end
 
 args = cmdline
 
-#server = "druby://#{args[:ipaddr]}:#{args[:port]}"
 server = "druby://:#{args[:port]}"
 
 DRb.start_service(server, Board.instance)
