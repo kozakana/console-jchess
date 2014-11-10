ebdf30d319958f4baedab49e1a9361ba3f9f15e2
mori-goki <mori-goki@kayac.com>
2014-11-10 22:47:16 +0900

    テストコード修正

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,25 +1,45 @@
 require_relative '../board'
 
+def init
+  data = Array.new(9).map{ Array.new(9, NilPiece.new) }
+  data[1][7] =  Kyo.new(:first, false)
+  data[4][4] =  Kaku.new(:first, false)
+  data[4][7] =  Hi.new(:first, false)
+  board_data = BoardData.instance
+  board_data.instance_eval do 
+    @data = data
+  end
+  @board = Board.instance
+  @board.instance_eval do
+    @data = board_data
+    @order_list[:first] = 1
+  end
+end
 
 describe Board do
-  describe "road_list" do
-    before do
-      data = Array.new(9).map{ Array.new(9, NilPiece.new) }
-      data[1][7] =  Kyo.new(:first, false)
-      data[4][4] =  Kaku.new(:first, false)
-      data[4][7] =  Hi.new(:first, false)
-      @board = Board.instance
-      @board.instance_eval do 
-        @data = data
+  #it "road_listの確認" do
+  #  pce = @board.piece [4,4]
+  #  #p @board.road_list pce, [4,7], [0,7]
+  #  p @board.move [4,7], [0,7], 1
+  #end
 
-        @order_list[:first] = 1
-      end
+  describe "香車" do
+    before { init }
+    it "moveのテスト" do
+     # expect do
+     #  p @board.move [1,7], [0,7], 1
+     # end.to raise_error( MissingPiece )
+       p @board.move [1,7], [1,6], 1
     end
+  end
+  
+  describe "飛車" do
 
-    it "road_listの確認" do
-      pce = @board.piece [4,4]
-      #p @board.road_list pce, [4,7], [0,7]
-      p @board.move [4,7], [0,7], 1
-    end
+
+  end
+
+  describe "角行" do
+
+
   end
 end
